d8740df9ddd20da0b3b43fb868184cd1b16b0655
VidhunM <vidhunalamelu18@gmail.com>
2026-02-26 13:02:51 +0530

    changes

--- src/components/Academy.jsx
+++ src/components/Academy.jsx
@@ -23,12 +23,12 @@ const Academy = () => {
         <section id="academy" className="py-12 sm:py-16 bg-gradient-to-b from-[#FFF9E1] to-white overflow-hidden relative">
             {/* Decorative background element - Top Left */}
             <div
-                className="absolute top-[-170px] left-[-90px] w-[600px] h-[600px] pointer-events-none opacity-[0.1] rotate-12 animate-slide-up-fade"
+                className="absolute top-[-170px] left-[-90px] w-[600px] h-[600px] pointer-events-none opacity-[0.5] rotate-12"
             >
                 <img
                     src="/images/abt.png"
                     alt=""
-                    className="w-full h-full object-contain mix-blend-multiply brightness-[0.1] contrast-[0.1]"
+                    className="w-full h-full object-contain mix-blend-multiply brightness-[0.5] contrast-[0.5]"
                 />
             </div>
 
