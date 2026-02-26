bb1c739cd52251c55530967b4ab916bb70fc81bc
VidhunM <vidhunalamelu18@gmail.com>
2026-02-26 12:53:38 +0530

    changes leaf

--- src/components/Academy.jsx
+++ src/components/Academy.jsx
@@ -23,24 +23,24 @@ const Academy = () => {
         <section id="academy" className="py-12 sm:py-16 bg-gradient-to-b from-[#FFF9E1] to-white overflow-hidden relative">
             {/* Decorative background element - Top Left */}
             <div
-                className="absolute top-[-170px] left-[-90px] w-[600px] h-[600px] pointer-events-none opacity-[0.7] rotate-12 animate-slide-up-fade"
+                className="absolute top-[-170px] left-[-90px] w-[600px] h-[600px] pointer-events-none opacity-[0.5] rotate-12 animate-slide-up-fade"
             >
                 <img
                     src="/images/abt.png"
                     alt=""
-                    className="w-full h-full object-contain mix-blend-multiply brightness-[0.6] contrast-[1.0]"
+                    className="w-full h-full object-contain mix-blend-multiply brightness-[0.5] contrast-[0.5]"
                 />
             </div>
 
             {/* Decorative background element - Bottom Right */}
             <div
-                className="absolute bottom-[30px] right-[-10px] w-[450px] h-[450px] pointer-events-none opacity-[0.7] animate-slide-up-fade"
+                className="absolute bottom-[30px] right-[-10px] w-[450px] h-[450px] pointer-events-none opacity-[0.5]"
             >
 
                 <img
                     src="/images/hml.png"
                     alt=""
-                    className="w-full h-full object-contain mix-blend-multiply brightness-[0.7] contrast-[1.0] transition-transform duration-700 hover:scale-105"
+                    className="w-full h-full object-contain mix-blend-multiply brightness-[0.5] contrast-[0.5]"
                 />
             </div>
 
