c2bc77ba238402fb1efae0701499d1b7a6b4f06e
Rathees Kumar <rathees2006@gmail.com>
2026-02-28 19:11:55 +0530

    Update CertificateListing.jsx

--- src/components/CertificateListing.jsx
+++ src/components/CertificateListing.jsx
@@ -170,13 +170,13 @@ const CertificateListing = () => {
                                             <span>({card.rating})</span>
                                             <div className="flex gap-[2px]">
                                                 {[...Array(5)].map((_, i) => (
-                                                    <svg key={i} width="14" height="14" viewBox="0 0 20 20" className="text-[#FCA65B]" fill="currentColor">
+                                                    <svg key={i} width="15" height="15" viewBox="0 0 20 20" className="text-[#FCA65B]" fill="currentColor">
                                                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                                                     </svg>
                                                 ))}
                                             </div>
                                         </div>
-                                        <div className="text-[12px] font-medium text-gray-800">
+                                        <div className="text-[16px] font-medium text-gray-800">
                                             Duration: {card.duration}
                                         </div>
                                     </div>
