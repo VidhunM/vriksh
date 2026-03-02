28539cabf3742b695821b4c1da8987f30fe227f5
VidhunM <vidhunalamelu18@gmail.com>
2026-03-02 11:25:04 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { FaWhatsapp } from 'react-icons/fa';
 import Header from './components/Header';
 import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
@@ -35,6 +36,17 @@ function AppContent() {
       </main>
       {!isAboutPage && <ContactFooter />}
       <Footer />
+
+      {/* Floating WhatsApp Icon */}
+      <a
+        href="https://wa.me/919880274824"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-6 right-6 lg:bottom-10 lg:right-10 w-14 h-14 bg-[#25D366] text-white rounded-full flex justify-center items-center shadow-[0_4px_14px_rgba(37,211,102,0.4)] hover:scale-110 transition-all z-[999]"
+        aria-label="Chat on WhatsApp"
+      >
+        <FaWhatsapp size={32} />
+      </a>
     </div>
   );
 }
